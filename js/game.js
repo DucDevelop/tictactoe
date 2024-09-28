832be38718e6de7abed8ac3b9f93638ecedd3a03
Duc <145208899+DucDevelop@users.noreply.github.com>
2024-09-28 13:44:30 +0200

    Working game in console

--- js/game.js
+++ js/game.js
@@ -2,6 +2,17 @@ function createBoard() {
 
     const GAME_FIELDS = 9;
     const MARKERS = ['X','O'];
+    const BOARD_ZONES = {
+        row_0: [0, 1, 2],
+        row_1: [3, 4, 5],
+        row_2: [6, 7, 8],
+        col_0: [0, 3, 6],
+        col_1: [1, 4, 7],
+        col_2: [2, 5, 8],
+        diag_0:[0, 4, 8],
+        diag_1:[2, 4, 6],
+    }
+
     let field = Array.from({length: GAME_FIELDS}, val => false);
     // winner mark or empty if draw
     let winner = '';
@@ -10,35 +21,36 @@ function createBoard() {
         let success = false;
 
         if(!field[cell] && marker && MARKERS.includes(marker.toUpperCase())) {
-            field[cell] = marker;
+            field[cell] = marker.toUpperCase();
             success = true;
         }
         return success;
     }
 
-    function checkGameOver() {
-        let isGameOver = false;
-        // board is completely occupied
-
+    function isFreeSpaceAvailable() {
         // cell unoccupied means cell === false else true
-        isGameOver = field.reduce((acc, val) => acc = acc && val? true : false ,true);
+        // callback: Boolean constructor will return true or false
+        return !field.every(Boolean);
+    }
 
-        if(!isGameOver) {
-            const winningCriteria = [
-                field[0] && field[0] === field[1] && field[0] === field[2],
-                field[3] && field[3] === field[4] && field[3] === field[5],
-                field[6] && field[6] === field[7] && field[6] === field[8],
-                field[0] && field[0] === field[3] && field[0] === field[6],
-                field[1] && field[1] === field[4] && field[1] === field[7],
-                field[2] && field[2] === field[5] && field[2] === field[8],
-                field[0] && field[0] === field[4] && field[0] === field[8],
-                field[6] && field[6] === field[4] && field[6] === field[2],
-            ]
-            winningCriteria.forEach(x => isGameOver = isGameOver || x);
+    function getWinner() {
+        let winningMark = '';
+        // loop over board zones and check winning criteria
+        for (const [_, fields] of Object.entries(BOARD_ZONES)) {
+            // field not empty to avoid comparing empty fields
+            if(field[fields[0]]) {
+                if(field[fields[0]]===field[fields[1]] && field[fields[0]] === field[fields[2]]) {
+                    winningMark = field[fields[0]];
+                    break;
+                }                    
+            }
         }
-            
-        // winning criteria is met
-        return isGameOver;
+        return winningMark;
+    }
+
+    function checkGameOver() {
+        // game over when either there is a winner or no space left
+        return Boolean(getWinner()) || !isFreeSpaceAvailable();     
     }
 
     function getBoardPretty() {
@@ -50,12 +62,15 @@ function createBoard() {
         console.log(getBoardPretty());
     }
 
-    function getWinner() {
-        return winner;
-    }
 
     function resetField() {
         field.forEach((_, idx) => field[idx] = false);
+    }
+
+    function getField(num) {
+        if(num >= 0 && num < GAME_FIELDS) {
+            return field[num]
+        }
     }
 
     return {
@@ -64,12 +79,16 @@ function createBoard() {
         checkGameOver,
         displayPretty,
         resetField,
+        isFreeSpaceAvailable,
+        getField,
     };
 }
 
 
-function createPlayer(name, marker) {
+function createPlayer(name, markerX=true) {
+
     let score = 0;
+    const marker = markerX? 'X' : 'O';
 
     function getScore() {
         return score;
@@ -77,19 +96,81 @@ function createPlayer(name, marker) {
     function setScore(val) {
         score = val;
     }
+    function getMarker() {
+        return marker;
+    }
+    function getName() {
+        return name;
+    }
 
     return {
-        name,
-        marker,
         getScore,
         setScore,
+        getMarker,
+        getName,
     };
 }
 
-let i = true;
+function createGame(player_name1, player_name2) {
+    const board = createBoard();
+    const players = [createPlayer(player_name1, true), createPlayer(player_name2, false)];
+    let playerTurn = 0;
 
-function sum(a, b) {
-    return a + b;
+    function playGame() {
+        resetGame();
+
+        do {
+            playRound();
+            playerTurn = (playerTurn + 1) % players.length;
+        } while(!board.checkGameOver())
+    
+        let winner = board.getWinner();
+        if(winner) {
+            // update score
+            players.forEach(x => {
+                if(x.getMarker() === winner) {
+                    x.setScore(x.getScore() + 1);
+                }
+            });
+        }
+        else {
+            // no winner
+        }
+    }
+
+    function playRound() {
+        // check if available fields
+        board.addMarker(getPlayerInput(players[playerTurn]), players[playerTurn].getMarker());
+
+        function getPlayerInput(player) {
+            let choice = 0;
+            board.displayPretty();
+            do {
+                // choice = Math.floor(Math.random() * 9);
+                choice = +prompt(`${player.getName()}: Which field?`);
+            } while(board.getField(choice))
+            return choice;
+        }
+
+    }
+
+    function resetGame() {
+        board.resetField();
+        playerTurn = 0;
+    }
+
+
+
+
+
+    return {
+        playGame,
+        playRound,
+        board,
+        players,
+    };
 }
 
-export {createBoard, sum, i};
+
+
+export {createBoard, createPlayer, createGame};
--- js/game.test.js
+++ js/game.test.js
@@ -1,6 +1,7 @@
-import {createBoard, i, sum} from './game'
+import {createBoard, createPlayer} from './game'
 
 const board = createBoard();
+
 
 describe('Check if game is over', () => {
     // Applies only to tests in this describe block
@@ -10,30 +11,163 @@ describe('Check if game is over', () => {
   
     test('Field full', () => {
       board.addMarker(0, 'X');
-      board.addMarker(1, 'X');
+      board.addMarker(1, 'O');
       board.addMarker(2, 'X');
       board.addMarker(3, 'X');
-      board.addMarker(4, 'X');
-      board.addMarker(5, 'X');
-      board.addMarker(6, 'X');
+      board.addMarker(4, 'O');
+      board.addMarker(5, 'O');
+      board.addMarker(6, 'O');
       board.addMarker(7, 'X');
       board.addMarker(8, 'X');
       board.addMarker(9, 'X');
 
       expect(board.checkGameOver()).toBe(true);
+      expect(board.getWinner()).toBe('');
+      expect(board.isFreeSpaceAvailable()).toBe(false);
     });
     test('1st row', () => {
       board.addMarker(0, 'X');
       board.addMarker(1, 'X');
       board.addMarker(2, 'X');
       expect(board.checkGameOver()).toBe(true);
+      expect(board.getWinner()).toBe('X');
+    });
+    test('2nd row', () => {
+      board.addMarker(3, 'O');
+      board.addMarker(4, 'O');
+      board.addMarker(5, 'O');
+      expect(board.checkGameOver()).toBe(true);
+      expect(board.getWinner()).toBe('O');
+    });
+    test('3rd row', () => {
+      board.addMarker(6, 'X');
+      board.addMarker(7, 'X');
+      board.addMarker(8, 'X');
+      expect(board.checkGameOver()).toBe(true);
+      expect(board.getWinner()).toBe('X');
+    });
+    test('1st column', () => {
+      board.addMarker(0, 'X');
+      board.addMarker(3, 'X');
+      board.addMarker(6, 'X');
+      expect(board.checkGameOver()).toBe(true);
+      expect(board.getWinner()).toBe('X');
+    });
+    test('2nd column', () => {
+      board.addMarker(1, 'X');
+      board.addMarker(4, 'X');
+      board.addMarker(7, 'X');
+      expect(board.checkGameOver()).toBe(true);
+      expect(board.getWinner()).toBe('X');
+    });
+    test('3rd column', () => {
+      board.addMarker(2, 'O');
+      board.addMarker(5, 'O');
+      board.addMarker(8, 'O');
+      expect(board.checkGameOver()).toBe(true);
+      expect(board.getWinner()).toBe('O');
     });
     test('1st row no win', () => {
       board.addMarker(0, 'X');
       board.addMarker(1, 'X');
       board.addMarker(2, 'O');
       expect(board.checkGameOver()).toBe(false);
+      expect(board.getWinner()).toBe('');
+    });
+    test('empty field', () => {
+      expect(board.checkGameOver()).toBe(false);
+      expect(board.getWinner()).toBe('');
     });
 
 });
 
+
+
+describe('Check Interaction', () => {
+    // Applies only to tests in this describe block
+    beforeEach(() => {
+        board.resetField()
+    });
+  
+    test('Add markers case insensitive', () => {
+      board.addMarker(0, 'X');
+      board.addMarker(1, 'O');
+      board.addMarker(2, 'X');
+      board.addMarker(3, 'x');
+      board.addMarker(4, 'o');
+
+      expect(board.getField(0)).toBe('X');
+      expect(board.getField(1)).toBe('O');
+      expect(board.getField(2)).toBe('X');
+      expect(board.getField(3)).toBe('X');
+      expect(board.getField(4)).toBe('O');
+    });
+
+    test('Add invalid markers', () => {
+      board.addMarker(0, 'X');
+      board.addMarker(1, 'P');
+      board.addMarker(2, 'X');
+
+      expect(board.getField(1)).toBe(false);
+    });
+
+    test('Get field out of range', () => {
+      expect(board.getField(-1)).toBeUndefined();
+      expect(board.getField(-9)).toBeUndefined();
+    });
+
+    test('Add markers on occupied field', () => {
+        board.addMarker(0, 'X');
+        expect(board.addMarker(0, 'O')).toBe(false);
+        expect(board.getField(0)).toBe('X');
+      });
+
+});
+
+
+describe('Check Player logic', () => {
+
+    test('Get functions', () => {
+        const name = 'P1';
+        const marker = false
+        const player1 = createPlayer(name, marker);
+        expect(player1.getName()).toBe(name);
+        expect(player1.getMarker()).toBe('O');
+        expect(player1.getScore()).toBe(0);
+    });
+    test('Default marker', () => {
+        const name = 'P1';
+        const player1 = createPlayer(name);
+        expect(player1.getMarker()).toBe('X');
+    });
+    test('O marker marker', () => {
+        const name = 'P1';
+        const markerX = false
+        const player1 = createPlayer(name, markerX);
+        expect(player1.getMarker()).toBe('O');
+    });
+
+    test('Set functions', () => {
+        const name = 'P1';
+        const player1 = createPlayer(name);
+        player1.setScore(3);
+        expect(player1.getScore()).toBe(3);
+        player1.setScore(player1.getScore()+1);
+        expect(player1.getScore()).toBe(4);
+    });
+
+});
+
+
+describe('Check Game logic', () => {
+    // Applies only to tests in this describe block
+    beforeEach(() => {
+        board.resetField()
+    });
+
+    test('Get field out of range', () => {
+      expect(board.getField(-1)).toBeUndefined();
+      expect(board.getField(-9)).toBeUndefined();
+    });
+
+});
